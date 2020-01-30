b4fba873a2cf32e00d1b653e06bb5700e3ba6275
HATS184 <zanegaffori@gmail.com>
2020-01-30 08:20:32 +0000

    update all

--- PyLibrary.py
+++ PyLibrary.py
@@ -1,1 +1,28 @@
+import json
+
+addjson = {}
+
+print('Welcome to PyLibrary cmd line. \n')
+
+def writefile(file, book, id):
+    with open(file, 'a') as i:
+        addjson[str(id)] = {"book": book}
+        json.dump(addjson, i)
+        
+
+while True:
+    cmd = input('> ')
+    if cmd == 'addbook':
+        print('What book wolud you like to add?')
+        cmd = input('> ')
+        xid = input('What id? >')
+        writefile('books.json', cmd, xid)
+    elif cmd == 'borrow':
+        xid = input('What is the id? >')
+        user = input('What is the user id?')
+        with open('users.json', 'a+') as i:
+            i = i.read()
+            xfile = json.loads(i)
+            xfile[str(xid)]["books"].append(xid)
+            json.dump(xfile, i)
 
